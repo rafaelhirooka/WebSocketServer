64439bb7b9744d8c1d2b6d3d601714c7e84bd896
Rafael Hirooka <rafaelhirooka@gmail.com>
2019-05-26 15:51:26 -0300

    allow external acces

--- app.php
+++ app.php
@@ -8,6 +8,6 @@ require __DIR__ . '/bootstrap/app.php';
 \Illuminate\Database\Capsule\Manager::table('active_connections')->delete();
 \Illuminate\Database\Capsule\Manager::table('subscribes')->delete();
 
-$app = new Ratchet\App($configW['host'], $configW['port']);
+$app = new Ratchet\App($configW['host'], $configW['port'], '0.0.0.0');
 $app->route('/', new \App\Sender($configS['secret']), array('*'));
 $app->run();
